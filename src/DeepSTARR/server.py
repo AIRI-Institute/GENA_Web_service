9a9fe88b2016e789f46b07746c4e5b05efcac7b3
Maxim Tavrickiy <m.tavrickiy@bk.ru>
2023-01-26 16:12:14 +0300

    Фикс путей

--- src/DeepSTARR/server.py
+++ src/DeepSTARR/server.py
@@ -68,7 +68,7 @@ def respond():
         get_model_prediction(req_path)
         save_annotations_files(pieces, chrome, req_path)
 
-        return jsonify({"path_to_bed_file":f"{req_path}/result.bed", "path_to_fasta_file":f"{req_path}/dna.fa", "path_to_fai_file":f"{req_path}/dna.fai"})
+        return jsonify({"acceptor_bed_file":'/generated/dnabert'+f"{req_path}/result.bed", "fasta_file":'/generated/dnabert'+f"{req_path}/dna.fa", "faidx_file":'/generated/dnabert'+f"{req_path}/dna.fa.fai"})
 
 
 if __name__ == "__main__":
