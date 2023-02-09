8f26e5169918d365fd1cd97f6ef057a5eb2cb3ac
Maxim Tavrickiy <m.tavrickiy@bk.ru>
2023-02-09 16:41:08 +0300

    #DNALM-5 Фикс путей

--- src/gena-spliceai/server.py
+++ src/gena-spliceai/server.py
@@ -92,8 +92,8 @@ def save_fasta_and_faidx_files(service_request: request, request_name: str) -> T
 
         # todo: убрать заглушку на обработку только одной последовательности в fasta файле, после того договоримся
         #  с фронтом как обрабатывать такие случаи
-        # respond_dict[f"{sample_name}_fasta_file"] = '/generated/' + file_name + '.fa'
-        respond_dict[f"fasta_file"] = '/generated/' + file_name + '.fa'
+        # respond_dict[f"{sample_name}_fasta_file"] = '/generated/gena-promoters_2000/' + file_name + '.fa'
+        respond_dict[f"fasta_file"] = '/generated/gena-spliceai/' + file_name + '.fa'
 
         # splice dna sequence to necessary pieces
         samples_queue[sample_name] = slicer(dna_seq, segment=conf.working_segment, step=conf.segment_step)
@@ -107,8 +107,8 @@ def save_fasta_and_faidx_files(service_request: request, request_name: str) -> T
         Faidx(respond_fa_file)
         # todo: убрать заглушку на обработку только одной последовательности в fasta файле, после того договоримся
         #  с фронтом как обрабатывать такие случаи
-        # respond_dict[f"{sample_name}_faidx_file"] = '/generated/' + file_name + '.fa.fai'
-        respond_dict[f"fai_file"] = '/generated/' + file_name + '.fa.fai'
+        # respond_dict[f"{sample_name}_faidx_file"] = '/generated/gena-spliceai/' + file_name + '.fa.fai'
+        respond_dict[f"fai_file"] = '/generated/gena-spliceai/' + file_name + '.fa.fai'
 
         total_time = time.time() - st_time
         logger.info(f"create and write {sample_name} faidx file exec time: {total_time:.3f}s")
@@ -147,8 +147,8 @@ def save_annotations_files(annotation: List[Dict],
     donor_file = respond_donor_file.open('w', encoding=coding_type)
 
     # add paths to files in respond dict
-    respond_dict['bed'].append('/generated/' + acceptor_file_name)
-    respond_dict['bed'].append('/generated/' + donor_file_name)
+    respond_dict['bed'].append('/generated/gena-spliceai/' + acceptor_file_name)
+    respond_dict['bed'].append('/generated/gena-spliceai/' + donor_file_name)
 
     # chr start end (записи только для позитивного класса)
     start = 0
