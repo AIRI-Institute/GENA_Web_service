4eededdf35a40d63867e452d4db848d8cbe84060
mks <maksimallist@gmail.com>
2023-02-27 20:19:18 +0300

    ref: add to gena-deepsea bed files seq description;

--- src/gena-deepsea/test_service.py
+++ src/gena-deepsea/test_service.py
@@ -6,14 +6,13 @@ from server import save_fasta_and_faidx_files, save_annotations_files, instance_
 
 if __name__ == "__main__":
     # read test fasta file
-    # request = '>chr1\nCTCGTTCCGCGCCCGCCATGGAACCGGATGTACGTTATAGCTATTACGCTACTGTGGGTGCACTCGTTCCGCGCCCGCCATGGAACCGGATGGTCTAGCCGATCTGACGCTCGTTCCGCGCCCGCCATGGAACCGGATGCCCCGCCCCTGGTTTCGAGTCGCTGGCCTGCTGGGTGTCATCGCATTATCGATATTGCATTACGTTATAGCTATTACCTCGTTCCGCGCCCGCCATGGAACCGGATGGCTACTGTGGGTGCAGTCTAGC'
-    test_file_path = service_folder.joinpath('data/checkpoints/ch11(mid).fa')
+    test_file_path = service_folder.joinpath('data/checkpoints/FlyBase_XEGBPL.fasta')
     with open(test_file_path, 'r', encoding='utf-8') as fasta:
         request = fasta.read()
 
     # fasta file processing
     request_name = f"request_{date.today()}_{datetime.now().microsecond}"
-    samples_queue, respond_dict = save_fasta_and_faidx_files(request, request_name)
+    samples_queue, respond_dict, description = save_fasta_and_faidx_files(request, request_name)
 
     # run model on inputs sequences
     respond_dict['bed'] = []
@@ -23,7 +22,7 @@ if __name__ == "__main__":
             sample_results.append(instance_class(batch))  # Dicts with list 'seq'
             # and 'prediction' vector of batch size
 
-        respond_dict = save_annotations_files(sample_results, sample_name, respond_dict, request_name)
+        respond_dict = save_annotations_files(sample_results, sample_name, respond_dict, request_name, description)
 
     # print output of the service
     for key, val in respond_dict.items():
